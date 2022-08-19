7e1636304cbf58de3f0ec807328d4bb70cb47710
Beon de Nood <77057717+beonde@users.noreply.github.com>
2022-08-19 12:01:28 -0400

    reproduce error code

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,45 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+  saveImage() {
+
+    this.httpClient.get('/assets/upload/bug.jpeg', {
+      observe: 'body',
+      responseType: 'blob'
+    }).subscribe(async blob => {
+
+      const data = await this.blobToBase64(blob);
+
+      Filesystem.writeFile({
+        path: 'test/bug.jpeg',
+        data,
+        directory: Directory.Cache
+      }).then(wfRes => {
+        console.log('COMPLETE!', wfRes);
+      }).catch(e => {
+        console.error('ERROR!', e);
+      });
+
+    });
+
+  }
+
+  private blobToBase64(blob) {
+    return new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onerror = reject;
+
+      reader.onloadend = () => {
+        const b64 = reader.result as string;
+        resolve(b64);
+      };
+      reader.readAsDataURL(blob);
+    });
+  }
 
 }
